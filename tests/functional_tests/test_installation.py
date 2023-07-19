6b3724c57ec7b7b757a3348824bcbae0b009f375
Rishu <rishu.singh@tigeranalytics.com>
2023-07-19 12:03:28 +0530

    addingfiles

---
+++ tests/functional_tests/test_installation.py
@@ -0,0 +1,9 @@
+import pytest
+
+def test_check_dependencies():
+# Add functional test for checking if all required dependencies are installed
+#  pass
+
+def test_missing_dependency():
+ # Add functional test for checking if the function correctly identifies a missing dependency
+ pass
